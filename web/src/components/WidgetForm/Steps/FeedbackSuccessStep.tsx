6da5ba4f3368b8ba444e88c32be5c39de9a8c8b1
Mauricio Girardi <maurigirarde@yahoo.com.br>
2022-05-04 20:00:22 -0300

    created FeedbackSuccessStep component

--- web/src/components/WidgetForm/Steps/FeedbackSuccessStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackSuccessStep.tsx
@@ -1,7 +1,30 @@
-export const FeedbackSuccessStep = () => {
+import { CloseButton } from "../../CloseButton";
+import successSvg from "../../../assets/svgs/ready.svg";
+
+type FeedbackSuccessStepProps = {
+  onFeedbackRestartRequest: () => void;
+};
+
+export const FeedbackSuccessStep = ({
+  onFeedbackRestartRequest,
+}: FeedbackSuccessStepProps) => {
   return (
-    <div>
-      <h1>Ola</h1>
-    </div>
+    <>
+      <header>
+        <CloseButton />
+      </header>
+
+      <div className="flex flex-col items-center py-10 w-[304px]">
+        <img src={successSvg} alt="icone de sucesso" />
+        <span className="text-xl mt-2">Agradecemos o feedback!</span>
+        <button
+          type="button"
+          onClick={onFeedbackRestartRequest}
+          className=" focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-brand-500 focus:ring-offset-zinc-900 py-2 px-6 mt-6 bg-zinc-800 rounded-md border-transparent text-sm leading-6 hover:bg-zinc-700 transition-colors"
+        >
+          Quero enviar outro
+        </button>
+      </div>
+    </>
   );
 };
